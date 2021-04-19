fcbf5cd0e0725ffcf13cfad0021e999d36a58f37
AlexSodreDev <alexandre.cortines@gmail.com>
2021-04-19 20:29:10 -0300

    add post to return test message for new user

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,15 @@ import express, { request, response } from "express";
 const app = express();
 
 app.get("/", (request, response) => {
-    return response.json({
-        message: "Olá NLW 05!",
-    });
+   return response.json({
+      message: "Olá NLW 05!",
+   });
 });
 
 app.post("/users", (request, response) => {
-    return response.json({
-        message: "Usuário salvo com sucesso!"
-    });
+   return response.json({
+      message: "Usuário salvo com sucesso!"
+   });
 });
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
