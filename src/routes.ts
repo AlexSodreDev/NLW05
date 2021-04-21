7b1a4062fb2c6dc4e1a0daff9353b4cc2f2c5d24
AlexSodreDev <alexandre.cortines@gmail.com>
2021-04-21 15:55:07 -0300

    add folder SettingsController

--- src/routes.ts
+++ src/routes.ts
@@ -6,18 +6,6 @@ import { SettingsRepository } from "./repositories/SettingsRepository";
 const routes = Router();
 
 routes.post("/settings", async (request, response) => {
-  const { chat, username } = request.body
-
-  const settingsRepository = getCustomRepository(SettingsRepository);
-
-  const settings = settingsRepository.create({
-    chat,
-    username
-  });
-
-  await settingsRepository.save(settings);
-
-  return response.json(settings);
 
 });
 
