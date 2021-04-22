5900c8ce38f03036e9375cf44d2aa8a1598fa5e5
AlexSodreDev <alexandre.cortines@gmail.com>
2021-04-21 22:04:57 -0300

    create a private for SettingsService

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,4 +1,5 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
 import { SettingsRepository } from "../repositories/SettingsRepository";
 
 interface ISettingsCreate {
@@ -14,9 +15,7 @@ class SettingsService {
   }
 
   async create({ chat, username }: ISettingsCreate) {
-    const settingsRepository = getCustomRepository(SettingsRepository);
-
-    const userAlreadyExists = await settingsRepository.findOne({
+    const userAlreadyExists = await this.settingsRepository.findOne({
       username
     });
 
@@ -24,12 +23,12 @@ class SettingsService {
       throw new Error("User already exists!");
     }
 
-    const settings = settingsRepository.create({
+    const settings = this.settingsRepository.create({
       chat,
       username
     });
 
-    await settingsRepository.save(settings);
+    await this.settingsRepository.save(settings);
 
     return settings;
   }
